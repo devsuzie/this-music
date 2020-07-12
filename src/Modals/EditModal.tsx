cff4165501712487837c7d90c8258488805d7935
suzie <suuuzie18@gmail.com>
2020-07-12 21:51:00 +0900

    Create modal component

--- src/Modals/EditModal.tsx
+++ src/Modals/EditModal.tsx
@@ -1,5 +1,44 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+import { ModalBody, ModalHeader, Modal } from "../components/Modal";
+import { useModalStore } from "../store";
+
+const theme = {
+  colors: {
+    primary: "#E6DEDE",
+    primaryLight: "#EDE7E7",
+    primaryDark: "#DBC9C9",
+    active: "#DDADAD",
+    highlight: "#1300FF",
+    white: "#FFFFFF",
+    black: "#121212",
+  },
+  fonts: {
+    futura: "Futura",
+    avenir: "Avenir",
+  },
+};
+
+const Title = styled.h3`
+  color: ${theme.colors.highlight};
+`;
 
 export default () => {
-  return <div>modal</div>;
+  const { closeModal } = useModalStore();
+
+  function handleClose() {
+    closeModal();
+  }
+
+  return (
+    <Modal>
+      <ModalHeader>
+        <Title>Edit</Title>
+      </ModalHeader>
+      <ModalBody>
+        <button onClick={handleClose}>닫기</button>
+      </ModalBody>
+    </Modal>
+  );
 };
---
+++ src/components/Modal.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+import _noop from "lodash/noop";
+import OriginModal from "react-bootstrap/Modal";
+
+import { useModalStore } from "../store";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const Modal: React.FC<{
+  onHide?: (...args: any[]) => void;
+  Header?: any;
+}> = ({ children, onHide = _noop, ...restProps }) => {
+  const { closeModal } = useModalStore();
+
+  function handleHide() {
+    onHide();
+    closeModal();
+  }
+
+  return (
+    <OriginModal
+      backdrop={false}
+      container={document.getElementById("root")}
+      onHide={handleHide}
+      show={true}
+      {...restProps}
+    >
+      {children}
+    </OriginModal>
+  );
+};
+
+export const ModalHeader = ({ children, ...restProps }: Props) => (
+  <OriginModal.Header closeButton {...restProps}>
+    {children}
+  </OriginModal.Header>
+);
+export const ModalBody = OriginModal.Body;
+export const ModalFooter = OriginModal.Footer;
