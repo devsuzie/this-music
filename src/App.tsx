a050c3a37f29613c227436125eeb35ae28953da5
suzie <suuuzie18@gmail.com>
2020-07-14 22:15:14 +0900

    Overide bootstrap style

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import { ThemeProvider } from "emotion-theming";
 import { BrowserRouter } from "react-router-dom";
 import { ModalProvider } from "./store";
-import theme from "./theme";
+import theme from "@/theme";
 
-import Router from "./Router";
+import Router from "@/Router";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@/theme/bootstrap.css";
 
 function App() {
   return (
